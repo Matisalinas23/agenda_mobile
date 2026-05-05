84bc552cc952ed43c3c7f1c7c1fea52f3933e3d0
Matias Salinas <matisalinas1602@gmail.com>
2026-05-05 19:11:24 -0300

    chore: add utility scripts for library verification

---
+++ scripts/check-lucide.js
@@ -0,0 +1,9 @@
+const lucide = require('lucide-react-native');
+const { User, Lock, Chrome, ArrowLeft } = lucide;
+
+console.log({
+  User: !!User,
+  Lock: !!Lock,
+  Chrome: !!Chrome,
+  ArrowLeft: !!ArrowLeft
+});
